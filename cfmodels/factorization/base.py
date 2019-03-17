2e6e6d9cfb4f5519c4113c189c8753d78c702d03
ubuntu <ubuntu@packer-ubuntu-16.04-desktop-gpu>
2019-03-17 07:33:26 +0100

    changed evaluation model not to use numba

--- cfmodels/factorization/base.py
+++ cfmodels/factorization/base.py
@@ -69,10 +69,11 @@ def is_metric_fit(metric, model):
 @nb.jit
 def predict(u, i, W, H):
     """"""
-    scores = np.zeros((len(i),), dtype=H.dtype)
-    for j in nb.prange(len(i)):
-        for r in range(W.shape[0]):
-            scores[j] += W[r, u] * H[r, i[j]]
+    # scores = np.zeros((len(i),), dtype=H.dtype)
+    # for j in nb.prange(len(i)):
+    #     for r in range(W.shape[0]):
+    #         scores[j] += W[r, u] * H[r, i[j]]
+    scores = W[:, u].T.dot(H).ravel()
     return scores
 
 
@@ -81,10 +82,7 @@ def predict(u, i, W, H):
 @nb.jit
 def predict_k(u, W, H, k):
     """"""
-    scores = np.zeros((H.shape[-1],), dtype=H.dtype)
-    for i in range(H.shape[-1]):
-        for r in range(W.shape[0]):
-            scores[i] += -W[r, u] * H[r, i]
+    scores = W[:, u].T.dot(H).ravel()
     ix = np.argpartition(scores, k)[:k]
     return ix[np.argsort(scores[ix])]
 
