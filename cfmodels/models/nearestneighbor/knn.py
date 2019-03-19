0b8fcc8d364ec8795c7d5a6b1f50a5b74c890fc9
Jaehun Kim <jaehun.j.kim@gmail.com>
2019-03-19 18:38:49 +0100

    add initial KNN module (currently nothing)

--- cfmodels/models/nearestneighbor/knn.py
+++ cfmodels/models/nearestneighbor/knn.py
@@ -1,5 +1,16 @@
 import numpy
+from ..base import CollaborativeFilteringBase
 from ...metrics import MetricBase, PointwiseMetric, RankingMetric
 from ...exceptions import MetricModelMismatch
 
 
+class UserKNN(CollaborativeFilteringBase):
+    """"""
+    def __init__(self, k, name='UserKNN'):
+        """"""
+        super().__init__(None, name)
+        self.k = k
+    
+    def fit_transform(self, Rtr, Rts=None, *params):
+        """"""
+        pass
